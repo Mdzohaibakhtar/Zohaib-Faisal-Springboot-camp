4ae89f08544a1eacccfb0f7390aaa74c9b31da95
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-07-09 12:12:40 +0530

    requestMapping

--- src/main/java/com/movie_Booking/demo/controller/PostController.java
+++ src/main/java/com/movie_Booking/demo/controller/PostController.java
@@ -6,9 +6,11 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/api")
 public class PostController {
 
     // Inject Service Layer Here
